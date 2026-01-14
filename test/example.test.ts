34ed30331876ca4d6fa2ea3006628dc2d801cee4
정규재 <wjdrbwo1206@naver.com>
2026-01-14 14:24:38 +0900

    add: prettier

---
+++ test/example.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, it} from "vitest";
+
+describe("Example", () => {
+    it("1+1 = 2", () => {
+        const result: number = 2;
+        expect(1+1).toBe(result);
+    })
+})
